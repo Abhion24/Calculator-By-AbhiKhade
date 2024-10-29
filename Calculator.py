0b7f3ae22454c79613a7467aca702558ed22c597
Abhion24 <128449028+Abhion24@users.noreply.github.com>
2024-10-29 11:36:58 +0530

    Simple Python Calculator by Abhi Khade
    This Python script is a simple calculator program designed by Abhi Khade. It performs basic arithmetic operations including addition, subtraction, multiplication, and division.
    
    User Interface Setup:
    
    The program starts by printing a decorative line to enhance the user interface.
    It then displays the title "Calculator" between two lines for clarity.
    Input Handling:
    
    The program prompts the user to enter two integer values. This is accomplished using the input() function, and the inputs are converted to integers using int() to ensure valid calculations.
    Calculations:
    
    After receiving the input values, the program performs the following calculations:
    Addition: Calculates the sum of the two values.
    Subtraction: Computes the difference between the first and second values.
    Multiplication: Multiplies the two values.
    Division: Divides the first value by the second, using floor division to return an integer result.
    Results Display:
    
    The results of the calculations are printed with clear labels, making it easy for the user to understand each operation's outcome.
    Program Closure:
    
    Finally, the program prints a thank you message surrounded by decorative lines, signifying the end of the calculation process.
    This script is an excellent example of how to create a user-friendly calculator in Python while implementing basic input/output functions and arithmetic operations.

---
+++ Calculator.py
@@ -0,0 +1,31 @@
+line = "---------------------------"   
+
+# First I'll make the Veriable for Shortcut 
+
+print(line)
+
+print("-----------------Calculator-----------------")
+
+print(line)
+
+# then i'll make it look Good 
+
+x = int(input("Enter The First Value :- "))
+y = int(input("Enter The Second Value :- "))
+
+# after that i need to get input in Integers That's why i used Int here 
+
+print(line)
+
+print("The Addition Will be :- ", x+y)
+print("The Substraction Will be :- ", x-y)
+print("The Multiply be :- ", x*y)
+print("The Division Will be :- ", x//y)
+
+# after that i'll set the Opretors for calculations 
+
+print(line)
+
+print(line,"Thank You ", line)
+
+# and then i'll print the results for them 
